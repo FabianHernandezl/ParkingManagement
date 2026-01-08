8f77c1e38befbe97cebdcf02849bb188d121807f
Fabian <fabianherlea@gmail.com>
2026-01-08 17:05:09 -0600

    Creación del proyecto

---
+++ src/main/java/view/ParkingManagement.java
@@ -0,0 +1,13 @@
+
+package view;
+
+/**
+ *
+ * @author fabia
+ */
+public class ParkingManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
