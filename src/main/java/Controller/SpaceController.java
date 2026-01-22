9d4c9c844c4cc9da5582f1484b28fa5eade8640a
Jimena Calvo <jimenacalvosoto13@gmail.com>
2026-01-21 20:13:25 -0600

    Clase space controller

---
+++ src/main/java/Controller/SpaceController.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import java.util.ArrayList;
+import model.data.SpaceData;
+import model.entity.Space;
+
+/**
+ *
+ * @author jimen
+ */
+public class SpaceController {
+
+    private SpaceData spaceData;
+
+    public SpaceController() {
+        spaceData = new SpaceData();
+    }
+
+    public String registerSpace(int id, boolean disability, boolean taken) {
+
+        Space space = new Space();
+        space.setId(id);
+        space.setDisabilityAdaptation(disability);
+        space.setSpaceTaken(taken);
+
+        return spaceData.insertSpace(space);
+    }
+
+    public ArrayList<Space> getAllSpaces() {
+        return spaceData.getAllSpaces();
+    }
+
+    public Space findSpaceById(int id) {
+        return spaceData.findSpaceById(id);
+    }
+
+    public String updateSpace(Space space) {
+
+        boolean updated = spaceData.updateSpace(space);
+        String message = "";
+
+        if (updated) {
+            message = "Espacio actualizado correctamente";
+        }
+        message = "No se pudo actualizar el espacio";
+        return message;
+    }
+
+    public String deleteSpace(int id) {
+
+        boolean deleted = spaceData.deleteSpace(id);
+        String message = "";
+
+        if (deleted) {
+            message = "Espacio eliminado correctamente";
+        }
+        message = "No se pudo eliminar el espacio";
+        return message;
+    }
+
+}
